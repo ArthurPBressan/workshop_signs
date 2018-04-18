b913eb722a1e76d7cb87ec78dd5eb9cab71dd827
Arthur Bressan <arthurbressan2@hotmail.com>
2018-04-18 16:19:35 -0300

    Implements generation

--- generate.py
+++ generate.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+import shutil
+import json
+
 jobs = ["blacksmith", "carpenter", "cook", "engineer", "herbalist", "mason", "potter", "weaver"]
 
 manifest = {
@@ -15,16 +19,46 @@ manifest = {
    "mixintos": {},
 }
 
-entity = {
-   "components": {
-      "stonehearth:workshop": {
-         "job_alias": "stonehearth:jobs:blacksmith"
-      }
-   }
-}
+def generate_entity(job):
+    job_alias = "stonehearth:jobs:{job}".format(job=job)
+    return {
+        "components": {
+            "stonehearth:workshop": {
+                "job_alias": job_alias
+            }
+        }
+    }
 
-mixinto = {
-    "stonehearth:decoration:wooden_sign_blacksmith": [
-        "file(entities/wooden_sign_blacksmith_workshop/)"
-    ]
-}
+
+def generate_mixinto(entity_pathname, job):
+    key = "stonehearth:decoration:wooden_sign_{job}".format(job=job)
+    file = "file({entity_pathname}/)".format(entity_pathname=entity_pathname)
+    return key, [file]
+
+def generate_filename(job):
+    return "wooden_sign_{job}_workshop".format(job=job)
+
+def main():
+    _dist = './dist'
+    shutil.rmtree(_dist, ignore_errors=True)
+    dist = Path(_dist)
+    dist.mkdir()
+
+    for job in jobs:
+        file_name = generate_filename(job)
+        entity_pathname = "entities/{file_name}".format(file_name=file_name)
+        mixinto_key, mixinto_files = generate_mixinto(entity_pathname, job)
+        manifest['mixintos'][mixinto_key] = mixinto_files
+
+        entity_dir = dist / entity_pathname
+        entity_dir.mkdir(parents=True)
+        entity_json = entity_dir / '{}.json'.format(file_name)
+        entity = generate_entity(job)
+        with entity_json.open(mode='w') as entity_file:
+            json.dump(entity, entity_file)
+
+    with (dist / 'manifest.json').open(mode='w') as manifest_file:
+        json.dump(manifest, manifest_file)
+
+if __name__ == '__main__':
+    main()
